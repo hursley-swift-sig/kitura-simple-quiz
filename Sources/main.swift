e6198fcf7c57d8eac90d43032e15fff417846fb4
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2016-07-22 16:43:53 +0100

    Change JSON value of correct to be a boolean rather than a string
    Also, make the question an actual question :)

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,8 +6,7 @@ let router = Router()
 router.all(middleware: BodyParser())
 
 router.get("/question") { req, res, next in
-    // FIXME This is not actually a question!
-    res.send(json: JSON([ "question": "As far as has ever been reported, no-one has ever seen an ostrich bury its head in the sand." ]))
+    res.send(json: JSON([ "question": "As far as has ever been reported, no-one has ever seen an ostrich bury its head in the sand. True or false?" ]))
     next()
 }
 
@@ -15,7 +14,7 @@ router.post("/answer") { req, res, next in
     switch req.body {
         case .text(let answer)?:
             let correct = (answer == "true")
-            res.send(json: JSON([ "correct": JSON(correct ? "true" : "false") ]))
+            res.send(json: [ "correct": correct as AnyObject ])
         default:
             res.status(.badRequest)
     }
